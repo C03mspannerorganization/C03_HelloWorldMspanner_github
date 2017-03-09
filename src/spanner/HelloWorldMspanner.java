f7bef3623949da61f5a3461e7a40a3598675c652
Mskanda <mskanda@student.tgm.ac.at>
2017-03-09 14:26:54 +0100

    Michi und Max

--- src/spanner/HelloWorldMspanner.java
+++ src/spanner/HelloWorldMspanner.java
@@ -5,6 +5,7 @@ public class HelloWorldMspanner {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Gruﬂ von Michi Sp.");
+		System.out.println("Gruﬂ von Max Sk.");
 	}
 
 }
