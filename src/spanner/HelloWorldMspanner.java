7c1739b5de2b5cc4781f0fac356d26caea80f16d
mspanner <mspanner@student.tgm.ac.at>
2017-03-09 13:56:42 +0100

    alles commiten

---
+++ src/spanner/HelloWorldMspanner.java
@@ -0,0 +1,10 @@
+package spanner;
+
+public class HelloWorldMspanner {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Gruﬂ von Michi Sp.");
+	}
+
+}
